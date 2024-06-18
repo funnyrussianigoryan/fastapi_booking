4d591af3409051c59716a27d285b1f7e31fe9182
PIL\PIL <pil@sinetic.ru>
2024-06-18 22:22:46 +0700

    exceptions bugfix

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,8 +1,34 @@
 from fastapi import HTTPException, status
 
 
-UserAlreadyExistsException = HTTPException(status_code=status.HTTP_409_CONFLICT, detail='User already exists')
-IncorrectEmailOrPwdException = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Incorrect email or password')
-TokenExpiredException = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Token has expired')
-TokenAbsentException = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Token absent')
-IncorrectTokenException = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Token is incorrect')
+class BookingException(HTTPException):
+    status_code = 500
+    detail = ""
+
+    def __init__(self):
+        super().__init__(status_code=self.status_code, detail=self.detail)
+
+
+class UserAlreadyExistsException(BookingException):
+    status_code = status.HTTP_409_CONFLICT
+    detail = "User already exists"
+
+
+class IncorrectEmailOrPwdException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = 'Incorrect email or password'
+
+
+class TokenExpiredException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = 'Token has expired'
+
+
+class TokenAbsentException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = 'Token absent'
+
+
+class IncorrectTokenException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = 'Token is incorrect'
